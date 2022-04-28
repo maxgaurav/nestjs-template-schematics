71eb0f69d0cace687a081a447710ad9f4e6654a7
Max Gaurav <max.gaurav@rubicotech.in>
2022-04-28 14:38:25 +0530

    add: basic schematic structure

---
+++ src/lib/defaults.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_AUTHOR = '';
+export const DEFAULT_DESCRIPTION = '';
+export const DEFAULT_LANGUAGE = 'ts';
+export const DEFAULT_VERSION = '0.0.1';
+export const PROJECT_TYPE = {
+  LIBRARY: 'library',
+  APPLICATION: 'application',
+};
